cbdf00fcfa5f1ab5ca11230fccb96867f56a2e70
angel-torres <alas4224@gmail.com>
2019-12-19 17:58:37 -0600

    added react-testing-library and @testing-library/jest-dom as dependencies

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import * as rtl from 'react-testing-library'
+import '@testing-library/jest-dom/extend-expect'
 import ReactDOM from 'react-dom';
 import App from './App';
 
