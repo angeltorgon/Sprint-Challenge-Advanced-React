c372229d8b25a37cc8f188c4189fa0638f063e7b
angel-torres <alas4224@gmail.com>
2019-12-19 18:07:58 -0600

    added @testing-library/react instead of react-testing-library

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import * as rtl from 'react-testing-library'
+import * as rtl from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper = rtl.render(
+    <App />
+  )
+  console.log(wrapper.debug())
 });
